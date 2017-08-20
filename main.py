d14833272685ab51afcf997917913bc0387bab23
bdbtravelling <bdbtravelling@gmail.com>
2017-08-20 20:51:00 +0100

    Updated the greeting

--- main.py
+++ main.py
@@ -8,11 +8,33 @@ margoinitstatus = init.margo_initialisation()
 print ("Margo initialised: " + margoinitstatus)
 
 """ Choose operating mode """
-operatingmode = init.choose_mode()
+""" operatingmode = init.choose_mode() """
+
+""" Variable setup"""
+conversation = False
 
 """ Enter Program State Machine """
 while True:
-    if operatingmode == "1":
+    if conversation:
+        receivedspeech = speech_processor.check_for_speech()
+        if "shopping" in receivedspeech:
+            speech_processor.speak_response("Ok, let's talk about shopping")
+        elif "reminder" in receivedspeech:
+            speech_processor.speak_response("Ok, let's check your reminders")
+        elif "company" in receivedspeech:
+            speech_processor.speak_response("Ok, I've got some ideas about that")
+        else:
+            speech_processor.speak_response("Sorry, I'm not sure what you mean")
+            conversation = False
+    else:
+        wakestatus = speech_processor.check_for_wake_signal()
+    if wakestatus:
+        """ Margo Wake - start dialogue"""
+        speech_processor.margo_greeting()
+        conversation = True
+        wakestatus = False
+
+"""    if operatingmode == "1":
         inputspeech = raw_input("Enter speech to process: ")
         if inputspeech == "Exit":
             break
@@ -24,6 +46,6 @@ while True:
             break
         speech_processor.speak_response(inputspeech)
     else:
-        break
+        break"""
 
-print ("Programme ending")
+print ("Program ending")
--- speech_processor.py
+++ speech_processor.py
@@ -1,5 +1,7 @@
 import nltk
 import tts_amazonpolly
+from datetime import datetime
+import time
 
 conversation = False
 
@@ -7,20 +9,40 @@ def process_speech(inputspeech):
     processedspeech = inputspeech + " - Speech Processed"
     tokens = nltk.word_tokenize(inputspeech)
     print (tokens)
-    if "shopping" in tokens:
-        print ("Enable shopping speech sequence")
     tagged = nltk.pos_tag(tokens)
     print (tagged[0:6])
     entities = nltk.chunk.ne_chunk(tagged)
     print (entities)
     return processedspeech
 
-def interpret_input(inputspeech):
+"""def interpret_input(inputspeech):
     wordtokens = nltk.word_tokenize(inputspeech)
     print (wordtokens)
-
     if "shopping" in wordtokens:
-        print ("Enable shopping speech sequence")
+        print ("Enable shopping speech sequence")"""
+
+def check_for_wake_signal():
+    listentext = raw_input("Enter Wake Signal (Hi Margo, Margo): ")
+    listentexttokens = nltk.word_tokenize(listentext)
+    print (listentexttokens)
+    if "Margo" in listentexttokens:
+        return True
+    else:
+        return False
+
+def check_for_speech():
+    listentext = raw_input("Enter speech: ")
+    listentexttokens = nltk.word_tokenize(listentext)
+    print (listentexttokens)
+    return listentexttokens
+
+def margo_greeting():
+    """thetime = str(datetime.now().time())"""
+    hour = time.strftime("%H")
+    print (hour)
+    """print (thetime)"""
+    speak_response("Hi!  How can I help?")
+    return
 
 def speak_response(inputspeech):
     tts_amazonpolly.convert_text_to_speach(inputspeech)
